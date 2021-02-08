86356aedd9764db22742040891326d7b6b144dec
tanzia-ahmed <tanziaahmed007@gmail.com>
2021-02-08 18:33:41 +0600

    initial setup for dev_map

--- src/main/java/team14/warzone/MapModule/Country.java
+++ src/main/java/team14/warzone/MapModule/Country.java
@@ -1,4 +1,15 @@
 package team14.warzone.MapModule;
 
+import team14.warzone.GameEngine.Player;
+
 public class Country {
+    private String  NAME;
+    private int ID;
+    private Player d_currentOwner;
+    private int NUMOFARMIES;
+
+    public Country(){
+
+    }
+
 }
