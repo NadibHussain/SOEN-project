d1fa6d6525291910a19cb198766d4053ee3c1a94
Anagh Mehran <anagh07@gmail.com>
2021-02-09 14:30:45 -0700

    setup junit and test folder for console

--- src/main/java/team14/warzone/Console/InputValidator.java
+++ src/main/java/team14/warzone/Console/InputValidator.java
@@ -1,4 +1,37 @@
 package team14.warzone.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class InputValidator {
+    public static enum Phase {
+        MAPEDITOR,
+        STARTUP,
+        GAMEPLAY
+    }
+
+    public static Phase d_CurrentPhase;
+    private static ArrayList<String> VALID_ORDER_LIST = new ArrayList<>(
+            Arrays.asList(
+                    "editcontinent",
+                    "editcountry",
+                    "editneighbor",
+                    "savemap",
+                    "editmap",
+                    "validatemap",
+                    "loadmap",
+                    "showmap",
+                    "gameplayer",
+                    "assigncountries",
+                    "deploy"
+            )
+    );
+
+    public boolean validateInput() throws Exception {
+        // check if p_InputMsg[0] exists in VALID_ORDER_LIST, if not throw exception
+
+        // switch to call method for each order
+
+        return true;
+    }
 }
---
+++ src/test/java/team14/warzone/Console/InputValidatorTest.java
@@ -0,0 +1,12 @@
+package team14.warzone.Console;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InputValidatorTest {
+
+    @Test
+    void validateInput() {
+    }
+}
