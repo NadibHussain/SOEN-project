f9018e5507fbf481c20a588f12646ba781e2de2f
Nadib <h.nadib@ferntechsolutions.com>
2021-02-09 23:21:00 +0600

    finished loadmap function and also added tests

--- src/main/java/team14/warzone/MapModule/MapEditor.java
+++ src/main/java/team14/warzone/MapModule/MapEditor.java
@@ -13,19 +13,78 @@ public class MapEditor {
 
     }
 
-    public void loadMap(String p_fileName){
+    public Map loadMap(String p_fileName){
+        Map map =  new Map();
         try {
             File myObj = new File(p_fileName);
             Scanner myReader = new Scanner(myObj);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                System.out.println(data);
+                if (data.equals("[countries]")){
+                    while (true){
+                        String l_line = myReader.nextLine();
+                        ArrayList<Continent> l_continentList = map.getD_continents();
+                        if(l_line.equals(""))
+                        {
+                            break;
+                        }
+                        else {
+                            String[] l_country_array = l_line.split(" ");
+                            String continentName= "";
+                            for (Continent l_conrinent: l_continentList) {
+                                if(l_conrinent.getID()==Integer.parseInt(l_country_array[2])){
+                                    continentName = l_conrinent.getName();
+                                }
+                            }
+                            map.addCountry(l_country_array[1],Integer.parseInt(l_country_array[0]),continentName);
+                        }
+                    }
+                }
+                else if (data.equals("[continents]"))
+                {
+                    int id = 1;
+                    while (true){
+                        String l_line = myReader.nextLine();
+                        if(l_line.equals(""))
+                        {
+                            break;
+                        }
+                        else {
+                            String[] l_continent_array = l_line.split(" ");
+                            map.addContinent(l_continent_array[0],id,Integer.parseInt(l_continent_array[1]));
+                            id++;
+                        }
+                    }
+                }
+                else if (data.equals("[borders]"))
+                {
+                    ArrayList<Country> l_countires = map.getD_countries();
+                    int l_index = 0;
+                    while (myReader.hasNextLine()){
+                        String l_line = myReader.nextLine();
+                        if(l_line.equals(""))
+                        {
+                            break;
+                        }
+                        else {
+                            String[] l_neighbour_array = l_line.split(" ");
+                            ArrayList<String> l_neighbourID_array = new ArrayList<String>();
+                            for (int x=1;x<l_neighbour_array.length;x++){
+                                l_neighbourID_array.add(l_neighbour_array[x]);
+                            }
+                            l_countires.get(l_index).setD_neighbours(l_neighbourID_array);
+                            l_index++;
+                        }
+                    }
+                }
+
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
+        return map;
 
     }
     public void saveMap(String p_fileName){
--- src/test/java/team14/warzone/MapModule/TestMapEditor.java
+++ src/test/java/team14/warzone/MapModule/TestMapEditor.java
@@ -1,4 +1,15 @@
 package team14.warzone.MapModule;
 
+import org.junit.jupiter.api.Test;
+
 public class TestMapEditor {
+    MapEditor me = new MapEditor();
+    @Test
+    void testLoadMap() {
+        Map m = me.loadMap("/home/nadib/bigeurope.map");
+        assert m.getD_continents().size() == 18;
+        assert m.getD_countries().size() == 180;
+        assert m.getD_countries().get(1).getD_neighbours().get(2).equals("8");
+    }
 }
+
