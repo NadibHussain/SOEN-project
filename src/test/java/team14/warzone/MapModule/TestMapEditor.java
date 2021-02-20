2b1a6727cf7941f9222d64a393e5d5a441fb9975
Nadib <h.nadib@ferntechsolutions.com>
2021-02-21 01:35:53 +0600

    added test in proper format

--- src/test/java/team14/warzone/MapModule/TestMapEditor.java
+++ src/test/java/team14/warzone/MapModule/TestMapEditor.java
@@ -2,25 +2,37 @@ package team14.warzone.MapModule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestMapEditor {
-    MapEditor me = new MapEditor();
 
-    @Test
-    void testLoadMap() {
-        me.loadMap("/home/nadib/bigeurope.map");
-        Map m = me.getD_loadedMap();
-        assert m.getD_continents().size() == 18;
-        assert m.getD_countries().size() == 180;
-        assert m.getD_countries().get(1).getD_neighbours().get(2).getD_CountryIntID() == 8;
+    public static MapEditor me;
+
+    @BeforeAll
+    public static void init(){
+        me = new MapEditor();
+        me.loadMap("/home/nadib/Desktop/soen project/SOEN-project/bigeurope.map");
     }
 
     @Test
-    void testValidateMap() {
-        me.loadMap("D:/Concordia Courses/SOEN 6441/Project Tools/bigeurope.map");
+    @DisplayName("Testing loading a map")
+    public void testLoadMap() {
+        Map m = me.getD_loadedMap();
+        assertEquals(m.getD_continents().size(),18);
+        assertEquals(m.getD_countries().size(),180);
+        assertEquals(m.getD_countries().get(1).getD_neighbours().get(2).getD_CountryIntID() ,8);
+    }
+
+    @Test
+    @DisplayName("Testing map validator")
+    public void testValidateMap() {
         Map m1 = me.getD_loadedMap();
         boolean test = me.validateMap(m1);
         assertEquals(true, test);
     }
+
+
 }
