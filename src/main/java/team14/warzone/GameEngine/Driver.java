124a7ddd42fbbd154cd10c1f6dd0392225fdd569
Anagh Mehran <anagh07@gmail.com>
2021-02-21 22:37:55 -0700

    Fixed GameEngine-Console dependency

--- src/main/java/team14/warzone/GameEngine/Driver.java
+++ src/main/java/team14/warzone/GameEngine/Driver.java
@@ -4,12 +4,12 @@ import team14.warzone.Console.Console;
 
 public class Driver {
     public static void main(String[] args) {
-        GameEngine ge = new GameEngine();
-        Console console = new Console(ge);
+        Console l_Console = new Console();
+        GameEngine l_GE = new GameEngine(l_Console);
 
         System.out.println("Welcome to warzone game, please enter your command : ");
         while (true) {
-            console.readInput();
+            l_Console.readInput();
         }
     }
 }
