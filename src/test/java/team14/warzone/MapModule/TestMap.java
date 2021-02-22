0b9dbaee294d0d37726323b1993b4e41c4a383c5
shaik raza <77544656+shaikraza02@users.noreply.github.com>
2021-02-23 04:35:10 +0530

    TestMap naming convention fixed

--- src/test/java/team14/warzone/MapModule/TestMap.java
+++ src/test/java/team14/warzone/MapModule/TestMap.java
@@ -9,31 +9,31 @@ import org.junit.jupiter.api.Test;
 
 public class TestMap {
 
-    public static Map m1;
-    public static Continent Asia, Africa;
-    public static Country India, Kenya;
+    public static Map d_M1;
+    public static Continent d_Asia, d_Africa;
+    public static Country d_India, d_Kenya;
 
     @BeforeAll
     public static void initialize() {
-        m1 = new Map();
-        Asia = new Continent(1, "Asia", 5);
-        Africa = new Continent(2, "Africa", 3);
-        India = new Country(1, "India", "Asia", null, 0);
-        Kenya = new Country(2, "Kenya", "Africa", null, 0);
+        d_M1 = new Map();
+        d_Asia = new Continent(1, "Asia", 5);
+        d_Africa = new Continent(2, "Africa", 3);
+        d_India = new Country(1, "India", "Asia", null, 0);
+        d_Kenya = new Country(2, "Kenya", "Africa", null, 0);
     }
 
     @Test
     @DisplayName("Testing Add Continent")
     public void testAddContinent() {
-        m1.addContinent("Asia", 5);
-        assertEquals("Asia", Asia.getD_ContinentID());
+        d_M1.addContinent("Asia", 5);
+        assertEquals("Asia", d_Asia.getD_ContinentID());
     }
 
     @Test
     @DisplayName("Testing Add Country")
     public void testAddCountry() {
-        m1.addCountry("India", "Asia");
-        assertEquals("India", India.getD_CountryID());
+        d_M1.addCountry("India", "Asia");
+        assertEquals("India", d_India.getD_CountryID());
     }
 
 }
