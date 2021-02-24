45c9957bf868a42ed0b66af9f86223d8c2c38843
Zeina Kittaneh <zeinakittaneh@gmail.com>
2021-02-24 18:48:10 -0500

    fix assigncountries

--- src/main/java/team14/warzone/GameEngine/GameEngine.java
+++ src/main/java/team14/warzone/GameEngine/GameEngine.java
@@ -88,11 +88,11 @@ public class GameEngine {
                 }
             }
             Console.displayMsg("Success: countries assigned");
+            // change phase to game play
+            InputValidator.CURRENT_PHASE = InputValidator.Phase.GAMEPLAY;
         } else {
             Console.displayMsg("Failed: 2-5 players required");
         }
-        // change phase to game play
-        InputValidator.CURRENT_PHASE = InputValidator.Phase.GAMEPLAY;
     }
 
     /**
