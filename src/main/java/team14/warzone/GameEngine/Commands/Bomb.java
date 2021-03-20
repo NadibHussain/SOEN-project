5a8e8d8378c34d6b862460c66ec3cfd278b72f71
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2021-03-21 02:12:08 +0600

    bomb and blockade done. blockade needs more to-do

--- src/main/java/team14/warzone/GameEngine/Commands/Bomb.java
+++ src/main/java/team14/warzone/GameEngine/Commands/Bomb.java
@@ -3,29 +3,57 @@ package team14.warzone.GameEngine.Commands;
 import java.util.ArrayList;
 
 import team14.warzone.GameEngine.GameEngine;
+import team14.warzone.GameEngine.Player;
 import team14.warzone.Console.Option;
+import team14.warzone.MapModule.Country;
+import team14.warzone.MapModule.Map;
 
 public class Bomb {
 
+    private String d_CountryNameTo;
+    private int d_NumberOfArmies;
     private GameEngine d_GameEngine;
-    private ArrayList<Option> d_OptionList;
 
     /**
      * Constructor of Bomb
+     * 
      * @author tanzia-ahmed
-     * @param p_OptionList
+     * @param p_CountryNameTo
+     * @param p_NumberOfArmies
+     * @param p_GameEngine
      */
-    public Bomb (ArrayList<Option> p_OptionList) {
-        d_OptionList = new ArrayList<>();
-        d_OptionList = p_OptionList;
+    public Bomb(String p_CountryNameTo, int p_NumberOfArmies, GameEngine p_GameEngine) {
+        this.d_CountryNameTo = p_CountryNameTo;
+        this.d_NumberOfArmies = p_NumberOfArmies;
+        this.d_GameEngine = p_GameEngine;
 
     }
 
     /**
      * Executes bomb command
      */
-    public void execute(){
+    public void execute() {
+        Map l_LoadedMap = d_GameEngine.getD_LoadedMap();
+
+        Player l_CurrentPlayer = d_GameEngine.getD_CurrentPlayer();
+        if (!l_CurrentPlayer.hasCard(new Card("Bomb"))) {// check if player has the Bomb card
+            throw new Exception("Player does not have this card.");
+        }
+        
+        // check if destination country exists
+        Country l_CountryTo = l_LoadedMap.findCountry(d_CountryNameTo);
+        if (l_CountryTo == null) {
+            throw new Exception("Bomb failed: destination country does not exist");
+        }
+        //check if destination country is owned by enemy player
+        if(l_CurrentPlayer.getD_CountriesOwned().contains(l_CountryTo) | l_CountryTo.getD_CurrentOwner() == "Neutral") {
+            throw new Exception("Bomb failed: " + l_CurrentPlayer.getD_Name() + " owns " +
+                    l_CountryTo.getD_CountryID());
+        }
+        else{
+            l_CountryTo.setD_NumberOfArmies(d_NumberOfArmies / 2);
+        }
 
     }
-    
+
 }
--- src/main/java/team14/warzone/GameEngine/Commands/Diplomacy.java
+++ src/main/java/team14/warzone/GameEngine/Commands/Diplomacy.java
@@ -3,28 +3,59 @@ package team14.warzone.GameEngine.Commands;
 import java.util.ArrayList;
 
 import team14.warzone.GameEngine.GameEngine;
+import team14.warzone.GameEngine.Player;
 import team14.warzone.Console.Option;
+import team14.warzone.MapModule.Country;
+import team14.warzone.MapModule.Map;
 
 public class Diplomacy {
+
+    private String d_CountryNameTo;
+    private int d_NumberOfArmies;
     private GameEngine d_GameEngine;
-    private ArrayList<Option> d_OptionList;
 
     /**
      * Constructor of Diplomacy
+     * 
      * @author tanzia-ahmed
-     * @param p_OptionList
+     * @param p_CountryNameTo
+     * @param p_NumberOfArmies
+     * @param p_GameEngine
      */
-    public Diplomacy (ArrayList<Option> p_OptionList) {
-        d_OptionList = new ArrayList<>();
-        d_OptionList = p_OptionList;
+    public Diplomacy(String p_CountryNameTo, int p_NumberOfArmies, GameEngine p_GameEngine) {
+        
+        this.d_CountryNameTo = p_CountryNameTo;
+        this.d_NumberOfArmies = p_NumberOfArmies;
+        this.d_GameEngine = p_GameEngine;
 
     }
 
     /**
      * Executes Diplomacy command
      */
-    public void execute(){
+    public void execute() {
+        Map l_LoadedMap = d_GameEngine.getD_LoadedMap();
 
+        Player l_CurrentPlayer = d_GameEngine.getD_CurrentPlayer();
+        if (!l_CurrentPlayer.hasCard(new Card("Diplomacy"))) {// check if player has the Bomb card
+            throw new Exception("Player does not have this card.");
+        }
+        
+        // check if destination country exists
+        Country l_CountryTo = l_LoadedMap.findCountry(d_CountryNameTo);
+        if (l_CountryTo == null) {
+            throw new Exception("Blockade failed: destination country does not exist");
+        }
+        // check if destination country is owned by enemy player
+        if (l_CurrentPlayer.getD_CountriesOwned().contains(l_CountryTo) | l_CountryTo.getD_CurrentOwner() == "Neutral") {
+            throw new Exception(
+                    "Blockade failed: " + l_CurrentPlayer.getD_Name() + " owns " + l_CountryTo.getD_CountryID() + " or it belongs to a neutral player.");
+        } else {
+            l_CountryTo.setD_NumberOfArmies(3*d_NumberOfArmies);
+            l_CountryTo.setD_CurrentOwner("Neutral");
+
+            //change back the country owner once the turn is finished.
+        }
     }
-    
+
 }
