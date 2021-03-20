6a249562d48fbe701e0a93137d964078af7a1d13
shaik raza <77544656+shaikraza02@users.noreply.github.com>
2021-03-20 10:36:24 +0530

    Comments added to subject and observer interfaces

--- src/main/java/team14/warzone/GameEngine/Observer.java
+++ src/main/java/team14/warzone/GameEngine/Observer.java
@@ -2,6 +2,13 @@ package team14.warzone.GameEngine;
 
 import java.util.ArrayList;
 
+/**
+ * This is the Observer Interface for implementing the Observer pattern
+ */
 public interface Observer {
+    /**
+     * This method updates the observer in case of any changes
+     * @param d_CountriesOwned The countries owned by a player
+     */
     public void update(ArrayList d_CountriesOwned);
 }
--- src/main/java/team14/warzone/GameEngine/Subject.java
+++ src/main/java/team14/warzone/GameEngine/Subject.java
@@ -4,10 +4,25 @@ import team14.warzone.MapModule.Country;
 
 import java.util.ArrayList;
 
+/**
+ * This is the Subject Interface for implementing the Observer pattern
+ */
 public interface Subject {
-
+    /**
+     * This is used to register observers to a subject
+     * @param o observer
+     */
     public void register(Observer o);
+
+    /**
+     * This is used to unregister observers to a subject
+     * @param o observer
+     */
     public void unregister(Observer o);
+
+    /**
+     * This is used to notify observers to a subject
+     */
     public void notifyObserver();
 
 
