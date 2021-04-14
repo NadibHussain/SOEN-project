57ff183c60cd6b1d6341096e8faa281da99095f6
Tanzia Ahmed <52648080+tanzia-ahmed@users.noreply.github.com>
2021-04-14 16:15:42 +0600

    unused import deleted

--- src/main/java/team14/warzone/GameEngine/State/Tournament.java
+++ src/main/java/team14/warzone/GameEngine/State/Tournament.java
@@ -2,7 +2,6 @@ package team14.warzone.GameEngine.State;
 
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
